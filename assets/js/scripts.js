ef542a8dc94ad28c150bdccadf601635311476e3
Ky <kylota.stewart@gmail.com>
2023-05-07 02:43:30 +0000

    SpaceXDragonv1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
 
-    $('.fade').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      slide: 'div',
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
-
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 
 });
+
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  } 
