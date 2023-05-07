6b6c2ef146d3d0bea4268a7c8de2b44927181f45
Ky <kylota.stewart@gmail.com>
2023-05-07 04:23:42 +0000

    everythingisbiggerintexasbecausetheyareblind

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -31,3 +31,13 @@ $(document).ready(function() {
   //slidesToScroll: 3
 //});
 });
+
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  } 
